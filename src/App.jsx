7dd607c94754f9753fae2b572b90cce7aa2ea8e4
Prakash9019 <plsprakash2003@gmail.com>
2025-02-24 10:30:53 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,8 @@ import Outreach from "./screens/outreach";
 import Questions from "./screens/questions";
 import Payment from "./screens/Payment/Payment";
 import PricingPage from "./screens/Subscription";
+import Cashfree from "./screens/Cashfree/cashfree";
+import Cashfree1 from "./screens/Cashfree/cashfree";
 function App() {
   return (
     <Routes>
@@ -28,13 +30,15 @@ function App() {
 
       <Route path="/outreach" element={<Outreach />} />
       <Route path="/explore" element={<Explore />} />
-      <Route path="/payment" element={<Payment />} />
+      {/* <Route path="/payment" element={<Payment />} /> */}
       <Route path="/sub" element={<PricingPage/> } />
       <Route path="/flow/outbound" element={<GenerateEmail />} />
       <Route path="/flow/chat" element={<Chat />} />
       <Route path="/flow/match flow" element={<Matchflow />} />
       <Route path="/flow/pipeline" element={<Pipeline />} />
       <Route path="/flow/docflow" element={<Docflow />} />
+
+      <Route path='/payment' element={<Cashfree1 />} />
 
       <Route path="/admin" element={<Admin />} />
 
---
+++ src/screens/Cashfree/cashfree.jsx
@@ -0,0 +1,103 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+// import '../paypal/paypal.css';
+import { data, useParams } from 'react-router-dom';
+
+import {load} from '@cashfreepayments/cashfree-js';
+
+const cashfree = await load({
+	mode: "production" //or production
+});
+
+const Cashfree1 = () => {
+    const params = useParams();
+    const isSessionId = params.sessionid;
+    const [loading, setLoading] = useState(false);
+    const [sessionId, setSessionId] = useState('');
+    const [orderId, setOrderId] = useState('');
+    
+    const getSessionId = async () => {
+        try {
+            setLoading(true);
+            const res = await axios.post('http://localhost:5000/payment/');
+            setLoading(false);
+            console.log("hellojh");
+            console.log(res);
+    
+            if (res.data.payment_session_id) {
+                setSessionId(res.data.payment_session_id);
+                console.log(res.data.payment_session_id);
+                setOrderId(res.data.order_id);
+                return res.data.payment_session_id;
+            } else {
+                throw new Error("Payment session ID not received");
+            }
+        } catch (err) {
+            setLoading(false);
+            console.log(err);
+            throw err;
+        }
+    };
+    
+    const handlePayment = async (e) => {
+        e.preventDefault();
+        try {
+            const paymentSessionId = await getSessionId();
+            console.log(paymentSessionId);
+            let checkoutOptions = {
+                paymentSessionId: paymentSessionId,
+                // returnUrl: "http://localhost:5000/api/status/{order_id}",
+                redirectTarget: "_modal",
+            };
+    
+            cashfree.checkout(checkoutOptions).then(function (result) {
+                console.log("Payment successful");
+                if (result.error) {
+                    alert(result.error.message);
+                }
+                if (result.redirect) {
+                    console.log("Redirection");
+                    console.log(result);
+                }
+            });
+        } catch (err) {
+            console.error("Error during payment:", err);
+        }
+    };
+    
+    useEffect(()=>{
+        setSessionId(isSessionId)
+    }, [isSessionId])
+
+  return (
+    <>
+    <div className='main'>
+        <div className='card px-5 py-4 mt-5'>
+
+            {/* <form onSubmit={getSessionId}>
+                <h1>Session Id</h1>
+                <input type="text" value={sessionId} onChange={(e)=>{setSessionId(e.target.value)}} />
+                {!loading? <div className='col-12 center'>
+                    <button className='w-100 ' type="submit">getSessionID</button>
+                </div>
+                :
+                <div className='col-12 center'>
+                    <button className='w-100 text-center' type="submit">
+                    <div className="spinner-border" role="status">
+                        <span className="visually-hidden ">Wait...</span>
+                    </div>
+                    </button>
+                </div>
+                }
+            </form> */}
+            <div className='col-12 center'>
+                <button className='w-100 ' type="submit" onClick={handlePayment}>Pay Now</button>
+            </div>
+        </div>
+    </div>
+   
+    </>
+  )
+}
+
+export default Cashfree1
