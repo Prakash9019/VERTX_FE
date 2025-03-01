093e5df098a5b6ffff89b455e281436825db8e5d
Prakash9019 <plsprakash2003@gmail.com>
2025-03-01 21:21:07 +0530

    added

--- key.js
+++ key.js
@@ -1,4 +1,4 @@
-const API_KEY = "https://govertx.com";
+const API_KEY = "https://vertx-server-eight.vercel.app";
 // https://vertx-server-eight.vercel.app/
 // const API_KEY = "http://localhost:5000";
 
