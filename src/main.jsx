3b9366df7212b62decdb7c6df30f07f9ef0ed836
TPK2006 <tanji4590@gmail.com>
2025-03-19 22:51:48 +0530

    19mar 10.51

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css';
 import {BrowserRouter} from "react-router"
-import App from './screens/inbox/inbox'
+import App from './App.jsx'
 import { CopyProtectionProvider } from "./context/CopyProtectionContext";
 
 createRoot(document.getElementById('root')).render(
