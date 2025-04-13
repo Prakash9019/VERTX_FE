43d0026e3509fff5a551775d4859c03bf2388029
Prakash9019 <plsprakash2003@gmail.com>
2025-04-13 20:09:05 +0530

    added

--- key.js
+++ key.js
@@ -1,4 +1,4 @@
-const API_KEY = "https://vertx-server-eight.vercel.app";
+const API_KEY = "https://vertx-server-817384216349.us-central1.run.app";
 export const Chat_key = "https://backend-chat-817384216349.us-central1.run.app"
 // https://vertx-server-eight.vercel.app/
 // const API_KEY = "http://localhost:5000";
