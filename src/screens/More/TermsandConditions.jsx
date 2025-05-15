81243f52cfba23046777b0f085049f688828c2dc
AnantaBG <anantabanikofficial@gmail.com>
2025-05-15 17:57:21 +0600

    Fixed 4 Bugs 3 major and 1 Minor

--- src/screens/More/TermsandConditions.jsx
+++ src/screens/More/TermsandConditions.jsx
@@ -15,7 +15,7 @@ const TermsAndConditions = ({onClose}) => {
                   // className="p-2  text-white bg-gray-800 rounded-full  hover:bg-gray-700 transition-colors"
                 >
                   <svg width="40" height="40" viewBox="0 0 32 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-<rect x="0.25" y="0.25" width="31.5" height="25.5" rx="12.75" stroke="#757575" stroke-width="0.5"/>
+<rect x="0.25" y="0.25" width="31.5" height="25.5" rx="12.75" stroke="#757575" strokeWidth="0.5"/>
 <path d="M12.9834 8.04688L15.7861 12.251L18.5957 8.04688H19.5938L16.2988 12.9482L19.6963 18H18.6914L15.7861 13.6592L12.8809 18H11.8828L15.2803 12.9482L11.9854 8.04688H12.9834Z" fill="#757575"/>
 </svg>
 
