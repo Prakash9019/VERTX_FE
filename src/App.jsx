8dd9e413866e4b00a424a47b8f2ddada8acb7350
Rohith <rohith.tr002@gmail.com>
2025-05-21 13:45:03 +0530

    changes applied

--- src/App.jsx
+++ src/App.jsx
@@ -101,7 +101,7 @@ function App() {
       <Route path="/privacy" element={<PrivacyPolicy /> } />
       <Route path="/terms" element={<TermsAndConditions /> } />
       <Route path="/settings" element={<SettingsV /> } />
-      <Route path="/help-center" element={<HelpCenterV /> } />
+      <Route path="/help-center" element={<HelpCenter /> } />
 
       {/* All routes in the About Folder */}
       <Route path="/help-center" element={<HelpCenter /> } />
