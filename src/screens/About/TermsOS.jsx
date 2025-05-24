b3ae09345792f68f62d9527760e5464e1bbdd536
AnantaBG <anantabanikofficial@gmail.com>
2025-05-24 13:33:58 +0600

    Report a problem and usenavigate carcus

--- src/screens/About/TermsOS.jsx
+++ src/screens/About/TermsOS.jsx
@@ -1,5 +1,7 @@
+import { useNavigate } from "react-router";
 
 const TermsOS = () => {
+  const navigate = useNavigate();
   return (
     <div className="fixed inset-0 bg-[#000000]/80 flex justify-center items-center z-50">
       <div className="w-[70%] max-sm:w-[95%] bg-black rounded-2xl border border-[#75757569] p-6 max-sm:p-2 pb-10 h-[95%] max-sm:h-[85%] overflow-hidden relative">
