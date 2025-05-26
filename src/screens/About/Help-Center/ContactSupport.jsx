0bd417eafc2421101989cecbae55ce8764c8eabd
AnantaBG <anantabanikofficial@gmail.com>
2025-05-26 12:17:35 +0600

    Feedback Carcu

--- src/screens/About/Help-Center/ContactSupport.jsx
+++ src/screens/About/Help-Center/ContactSupport.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useNavigate } from 'react-router';
 
 const ContactSupport = () => {
+  const navigate = useNavigate();
   return (
     <div className='text-white'>ContactSupport</div>
   )
