88d1b726f6562d23a44f33d42c1331236cd1ef15
Prakash9019 <plsprakash2003@gmail.com>
2025-06-08 11:43:52 +0530

    added

--- src/screens/layout/bars.jsx
+++ src/screens/layout/bars.jsx
@@ -618,6 +618,14 @@ export function Sidebar({ sidebarOpen, setSidebarOpen }) {
                   >
                     Log in
                   </button>
+                  <div className="flex justify-center w-full mb-6">
+                    <button
+                      onClick={() => navigate("/privacy")}
+                      className="text-md text-gray-400 hover:text-white underline"
+                    >
+                      Privacy Policy
+                    </button>
+                  </div>
                 </div>
               )}
             </div>
