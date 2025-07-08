cd601e19d96c6b2592b7e38b79431c69ea365a88
awanish kumar yadav <aay14155@gmail.com>
2025-07-08 22:36:53 +0530

    done

--- key.js
+++ key.js
@@ -1,7 +1,9 @@
-const API_KEY = "https://auth.govertx.com"; 
-export const Chat_key = "https://chat.govertx.com"
+const API_KEY = "https://vertx-server-817384216349.us-central1.run.apps";
+export const Chat_key = "https://backend-chat-817384216349.us-central1.run.app";
 // https://vertx-server-eight.vercel.app/
 // export const API_KEY = "http://localhost:5000";
 // export const Chat_key = "http://localhost:5001"
 
 export default API_KEY;
+//const API_KEY = "https://vertx-server-817384216349.us-central1.run.app";
+//https://backend-chat-817384216349.us-central1.run.app
