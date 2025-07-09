85e76c3bcc8a39fe04096bb1d5df665d1246858d
awanish kumar yadav <aay14155@gmail.com>
2025-07-09 12:23:47 +0530

    fix url

--- key.js
+++ key.js
@@ -1,7 +1,7 @@
-const API_KEY = "https://vertx-server-817384216349.us-central1.run.apps";
+const API_KEY = "https://vertx-server-817384216349.us-central1.run.app";
 export const Chat_key = "https://backend-chat-817384216349.us-central1.run.app";
 // https://vertx-server-eight.vercel.app/
-// export const API_KEY = "http://localhost:5000";
+ //export const API_KEY = "http://localhost:5000";
 // export const Chat_key = "http://localhost:5001"
 
 export default API_KEY;
